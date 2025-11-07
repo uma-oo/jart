845c5c7da16adace0bbbfde33ed9df814320f3f9
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-07 14:27:02 +0100

    add some code

---
+++ geometrical_shapes/Point.java
@@ -0,0 +1,19 @@
+
+public class Point {
+
+    public int x;
+    public int y;
+
+    Point() {
+        this(0, 0);
+    }
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    
+
+}
